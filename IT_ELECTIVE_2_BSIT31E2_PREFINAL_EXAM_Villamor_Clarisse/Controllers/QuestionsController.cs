2da47e91dad9ab216efc14822d6d577df7d953b9
minxiaa <hanayopotato@gmail.com>
2026-09-12 07:13:08 +0800

    Question 1

---
+++ IT_ELECTIVE_2_BSIT31E2_PREFINAL_EXAM_Villamor_Clarisse/Controllers/QuestionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT_ELECTIVE_2_BSIT31E2_PREFINAL_EXAM_Villamor_Clarisse.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
